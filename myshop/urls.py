82c346af5dfff6d92830fb8201dbbdd7af8327c3
Your Name <you@example.com>
2019-07-24 19:32:16 +0800

    add the cart function

--- myshop/urls.py
+++ myshop/urls.py
@@ -17,10 +17,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-
+from cart import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('cart/', include('cart.urls', namespace='cart')),
     path('', include('shop.urls', namespace='shop')),
 ]
 
